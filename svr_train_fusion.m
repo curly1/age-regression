de6cb4af905f379cd9f25f3ec85aa3a7cc30e05e
Joanna Grzybowska <jgrzybowska@phaser>
2016-02-24 15:24:57 +0100

    Detrend added

--- svr_train_fusion.m
+++ svr_train_fusion.m
@@ -1,12 +1,14 @@
-clear, close all;
+%clear, close all;
 
-addpath([cd '/code'])
-addpath([cd '/code/LSSVM/LSSVMlabv1_8_R2009b_R2011a'])
-data1 = load('aGender_ProsodicParams.mat');
-data2 = load('aGender_ivec.mat');
+path_to_data = '/storage/dane/jgrzybowska/MATLAB/ivectors/age_regression/data';
+addpath(path_to_data);
+addpath([cd '/LSSVM/LSSVMlabv1_8_R2009b_R2011a'])
+data1 = load('aGender_ivec_400_TUBMz_agender_german_ubm_PitchParams12D_1s.mat');
+data2 = load('aGender_ivec_TUBMzYT_MFCC60D.mat');
+load('fixed_folds_aGender_K15.mat');
 
 %% SETTINGS
-minAge = 0;
+minAge = 6;
 maxAge = 80;
 %group = (data2.females);
 group = (data2.males | data2.females | data2.children);
@@ -14,8 +16,8 @@ kernel = 'RBF_kernel';
 
 %% make sure ids of data1 = ids of data2
 
-ids1 = cell2mat(data1.params(:,2));
-ids2 = data2.ids(group);
+ids1 = who3;
+ids2 = w;
 
 % sort by id
 [ids1,i1] = sort(ids1);
@@ -35,9 +37,11 @@ X2 = data2.features(idx2,:);
 Y2 = data2.labels(idx2);
 
 %%
-K = 10; 
+K = 15; 
 
-folds = crossvalind('Kfold', size(X1,1), K);                 % randomowo czesc danych do testu
+%folds = crossvalind('Kfold', size(X1,1), K);                 % randomowo czesc danych do testu
+
+folds = folds3;
 
 for k = 1:K
 
@@ -49,12 +53,12 @@ type = 'function estimation';
 %% 1st model
 %[gam1,sig21] = tunelssvm({X1(train_idx,:),Y1(train_idx),type,[],[], kernel},...
 %    'simplex', 'leaveoneoutlssvm',{'mse'});
-gam1 = 23.7135;     % RBF
-sig21 = 36.4475;
+gam = gam3(k);     % RBF
+sig2 = sig23(k);
 
-[alpha1,b1] = trainlssvm({X1(train_idx,:),Y1(train_idx),type,gam1,sig21, kernel});
+[alpha1,b1] = trainlssvm({X1(train_idx,:),Y1(train_idx),type,gam,sig2, kernel});
 
-Y_pred1 = simlssvm({X1(train_idx,:),Y1(train_idx),type,gam1,sig21, kernel ,'preprocess'},...
+Y_pred1 = simlssvm({X1(train_idx,:),Y1(train_idx),type,gam,sig2, kernel ,'preprocess'},...
     {alpha1,b1},X1(test_idx,:));
 Y_pred1(Y_pred1 < minAge) = minAge;
 Y_pred1(Y_pred1 > maxAge) = maxAge;
@@ -64,18 +68,23 @@ Y_true1 = Y1(test_idx);
 
 Y_pred1 = Y_pred1(idx);
 
+% usuwanie trendu
+lin_func = polyfit((1:length(Y_true1))', Y_true1, 1);
+Y_true1_lin_func = Y_true1 - polyval(lin_func, (1:length(Y_true1))')+mean(Y_true1);
+%Y_pred1_lin_func = Y_pred1 - polyval(lin_func, (1:length(Y_true1))');
+
+MAE1_lin_func = 1/(length(Y_pred1))*sum(abs(Y_pred1-Y_true1_lin_func));
 MAE1 = 1/(length(Y_pred1))*sum(abs(Y_pred1-Y_true1));
 MAE_std1 = std(abs(Y_pred1-Y_true1));
 
 %% 2nd model
-%[gam2,sig22] = tunelssvm({X2(train_idx,:),Y2(train_idx),type,[],[], kernel},...
+%[g(k),s2(k)] = tunelssvm({X2(train_idx,:),Y2(train_idx),type,[],[], kernel},...
 %    'simplex', 'leaveoneoutlssvm',{'mse'});
-gam2 = 7.760218;     % RBF
-sig22 = 820.3196;
+gam = g(k);
+sig2 = s2(k);
+[alpha2,b2] = trainlssvm({X2(train_idx,:),Y2(train_idx),type,gam,sig2, kernel});
 
-[alpha2,b2] = trainlssvm({X2(train_idx,:),Y2(train_idx),type,gam2,sig22, kernel});
-
-Y_pred2 = simlssvm({X2(train_idx,:),Y2(train_idx),type,gam2,sig22, kernel ,'preprocess'},...
+Y_pred2 = simlssvm({X2(train_idx,:),Y2(train_idx),type,gam,sig2, kernel ,'preprocess'},...
     {alpha2,b2},X2(test_idx,:));
 Y_pred2(Y_pred2 < minAge) = minAge;
 Y_pred2(Y_pred2 > maxAge) = maxAge;
@@ -89,8 +98,10 @@ MAE2 = 1/(length(Y_pred2))*sum(abs(Y_pred2-Y_true2));
 MAE_std2 = std(abs(Y_pred2-Y_true2));
 
 %% fusion
-Y_pred = (0.2*Y_pred1) + (0.8*Y_pred2);
+Y_pred = (0.1*Y_pred1) + (0.9*Y_pred2);
 if Y_true1 == Y_true2, Y_true = Y_true1; end
+%%
+
 MAE_fused = 1/(length(Y_pred))*sum(abs(Y_pred-Y_true));
 MAE_std_fused = std(abs(Y_pred-Y_true));
 
@@ -99,17 +110,34 @@ p1 = corrcoef([Y_true, Y_pred1]);
 p2 = corrcoef([Y_true, Y_pred2]);
 p = corrcoef([Y_true, Y_pred]);
 
-[MAE_fused < MAE2 , p(2) > p2(2)]
+scores = [MAE_fused < MAE2 , p(2) > p2(2)];
 
 p_ref = 1/(length(Y_pred)-1)*((Y_true-mean(Y_true))/std(Y_true))'*((Y_pred-mean(Y_pred))/std(Y_pred));
 
 pearson(k) = p(2) - p2(2);
 MAE_diff(k) = MAE2 - MAE_fused;
 
+MAE1Detrend(k) = MAE1_lin_func;
+MAE_fu(k) = MAE_fused;
+MAE1k(k) = MAE1;
+MAE2k(k) = MAE2;
+pk(k) = p(2);
+p1k(k) = p1(2);
+p2k(k) = p2(2);
+
+
 end
 
-P = mean(pearson)
-MAE = mean(MAE_diff)
+P = mean(pearson);
+MAE = mean(MAE_diff);
+
+mean(MAE1Detrend)
+mean(MAE_fu)
+mean(MAE1k)
+mean(MAE2k)
+mean(pk)
+mean(p1k)
+mean(p2k)
 
 %%
 figure()
@@ -126,12 +154,12 @@ legend('True', 'Predicted')
 ylabel('age'), xlabel('# speaker')
 title('model 2')
 
-figure()
-plot(Y_true); hold on
-plot(Y_pred, 'or')
-legend('True', 'Predicted')
-ylabel('age'), xlabel('# speaker')
-title('fused model')
+% figure()
+% plot(Y_true); hold on
+% plot(Y_pred, 'or')
+% legend('True', 'Predicted')
+% ylabel('age'), xlabel('# speaker')
+% title('fused model')
 
-rmpath([cd '/code'])
-rmpath([cd '/code/LSSVM/LSSVMlabv1_8_R2009b_R2011a'])
+rmpath([cd '/LSSVM/LSSVMlabv1_8_R2009b_R2011a'])
+rmpath(path_to_data);
